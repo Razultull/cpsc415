d9cd881b83ada5cbbc93bc0b324fc17d620edf26
Razultull <razultull@gmail.com>
2013-02-11 16:09:57 -0500

    Project Restarted - Renamed to Depot
    Restarted project

--- First/demo/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Demo::Application.config.secret_token = 'f1d664eaa8fe7c99054daabe0daf8bfeb9ec18fd94563fe740b3211bbeb99f92924aa330b70ee0cd2ea4230836fad6671b6b12b3504f725b54ae7631bfaad88f'
---
+++ depot/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = 'e80fabb9f4a2c259d8bd3f1769aacd0d5dfbea63845855ca2fd83ea5deb9b558428d951f41dfcf28e0e7d87c0f84e372826579a86f3fdbf18bf82a4e483569a1'
