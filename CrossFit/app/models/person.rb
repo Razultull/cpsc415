b0a812b0ac03276ba809c3f44e48798c930e3f26
Razultull <razultull@gmail.com>
2013-04-02 17:09:50 -0400

    Made new project
    hosted on heroku and deployed, boostrap integration soon to come, user
    sign in and management complete, comments and views next.

--- CrossFit/app/models/person.rb
+++ CrossFit/app/models/person.rb
@@ -1,3 +1,7 @@
 class Person < ActiveRecord::Base
   attr_accessible :address, :date_of_birth, :height, :name, :sex, :weight
+  validates :address, :date_of_birth, :height, :name, :sex, :weight, :presence => true
+  validates :name, :uniqueness =>true
+  has_one :Gymnasium
+  has_many :workouts, inverse_of: :Person
 end
